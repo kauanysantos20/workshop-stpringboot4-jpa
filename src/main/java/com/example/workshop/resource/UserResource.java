96c8a4cc85df694a5ce041057096e4199b44c16b
kauanysantos20 <kauanysantos.ls04@gmail.com>
2026-01-26 19:50:06 -0300

    User insert

--- src/main/java/com/example/workshop/resource/UserResource.java
+++ src/main/java/com/example/workshop/resource/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.workshop.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.workshop.entities.User;
 import com.example.workshop.services.UserService;
@@ -18,6 +22,7 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List <User>> findAll(){
 		List<User> list = service.findAll();
@@ -30,5 +35,12 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
 
--- src/main/java/com/example/workshop/services/UserService.java
+++ src/main/java/com/example/workshop/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
 		Optional <User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	//metodo para salvar no BD um dado usuário
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
